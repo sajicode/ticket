b7d36a7570fafd7d04e5f259c2262558f71b623d
sajicode <sajioloye@gmail.com>
2019-01-10 12:07:41 +0100

    add route to get all tickets

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -19,4 +19,14 @@ module.exports = (app) => {
 			res.status(422).send(error);
 		}
 	});
+
+	app.get('/api/ticket', async (req, res) => {
+		const tickets = await Ticket.find().populate('ticketType');
+
+		try {
+			res.status(200).send(tickets);
+		} catch (error) {
+			res.status(400).send(error);
+		}
+	});
 };
